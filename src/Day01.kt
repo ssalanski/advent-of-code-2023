f6fe3f89ec17dc021462916553fb57e5077c8a1e
Stefan Salanski <stefan.salanski@sapns2.com>
2023-12-17 12:43:51 -0500

    day 01 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,22 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+  fun part1(input: List<String>): Int {
+    return input.sumOf { line ->
+      line
+        .filter { char -> char.isDigit() }
+        .let { it.first().digitToInt() * 10 + it.last().digitToInt() }
     }
+  }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+  fun part2(input: List<String>): Int {
+    return input.size
+  }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInput("Day01_test")
+  println(testInput)
+  check(part1(testInput) == 142)
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+  val input = readInput("Day01")
+  part1(input).println()
+  part2(input).println()
 }
